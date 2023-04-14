d7e00335ec02dd5efd7f643282663ba4c54a48d4
pelmen964 <ivanklimov41234@gmail.com>
2023-04-14 11:41:22 +0700

    1.01

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 import uvicorn
 app = FastAPI()
 # Hello World route
+def sum_two_args(x,y):
+ return x+y
+# Hello World route
 @app.get("/")
 def read_root():
- return {"Hello": "World"}
+   return {"Hello": "World"}
 if __name__ == "__main__":
- uvicorn.run(app, host="0.0.0.0", port=8000)
+   uvicorn.run(app, host="0.0.0.0", port=8000)
