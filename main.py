7872266a69e0967eae5594bd4f2715f3008d719f
pelmen964 <ivanklimov41234@gmail.com>
2023-04-14 10:10:57 +0700

    add

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+# Hello World route
+@app.get("/")
+def read_root():
+ return {"Hello": "World"}
+if __name__ == "__main__":
+ uvicorn.run(app, host="0.0.0.0", port=8000)
