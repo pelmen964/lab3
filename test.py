4952a09a3687295584b3ac8618c4d31800ce8f09
pelmen964 <ivanklimov41234@gmail.com>
2023-04-14 12:15:40 +0700

    added tests fix

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import fastlab
+import main
 def test1():
- assert fastlab.sum_two_args(2,2) == 4
+ assert main.sum_two_args(2,2) == 4
 def test2():
- assert fastlab.sum_two_args(2.0001,2) == 4
+ assert main.sum_two_args(2.0001,2) == 4
 def test3():
- assert fastlab.sum_two_args(3,2) == 5
+ assert main.sum_two_args(3,2) == 5
 def test4():
- assert fastlab.sum_two_args("2",2) == 4
+ assert main.sum_two_args("2",2) == 4
