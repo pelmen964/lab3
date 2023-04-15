229be89581ead6b9895ff5a3eae95d821db4c9b1
pelmen964 <ivanklimov41234@gmail.com>
2023-04-15 19:43:56 +0700

    added normal tests

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import main
 def test1():
- assert main.sum_two_args(2,2) == 4
+ pass
 def test2():
- assert main.sum_two_args(2.0001,2) == 4
+ assert main.sum_two_args(2.0001,2) == 4.0001
 def test3():
  assert main.sum_two_args(3,2) == 5
 def test4():
- assert main.sum_two_args("2",2) == 4
+ assert main.sum_two_args("2","2") == "22"
